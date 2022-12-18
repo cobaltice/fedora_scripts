54bf9bdee1fa942520ab1ceeb9ac333d83235c3b
cobaltice <120961519+cobaltice@users.noreply.github.com>
2022-12-18 16:39:08 +0000

    Add files via upload

---
+++ setup-rpmfusion.sh
@@ -0,0 +1,1 @@
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
